de413cac7bf85f9e66def28528981fa527cd0989
Gabriel <gabriel.rdrguez@gmail.com>
2016-07-15 18:01:10 -0300

    language almost done

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,7 +2,10 @@ class Room < ActiveRecord::Base
   def complete_name
     "#{title}, #{location}"
   end
-end
-def Room_params
-  parans.require(:title).permit(:description, :location, :title)
+
+
+  validates_presence_of :title, :location
+
+
+
 end
